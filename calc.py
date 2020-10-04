968ac1efd04fe56162b7d1ff31294ce17a36fc95
zerasul <zerasul@gmail.com>
2020-10-04 12:42:33 +0200

    añadiendo primer fichero de ejemplo y gitignore

---
+++ calc.py
@@ -0,0 +1,27 @@
+class Calc:
+"""
+clase Calc; permite sumar, restar, multiplicar y dividir
+>>> calc = Calc()
+"""
+    def suma(self,a,b):
+        """Esta funcion recibe dos numeros y devuelve la suma
+        >>> calc.suma(2,2)
+        4
+        """
+        pass
+    def resta(self,a,b):
+        """Esta funcion recibe dos numeros y devuelve la resta
+        >>> calc.resta(2,2)
+        0
+        """
+        pass
+    def mult(self,a,b):
+        """Esta funcion recibe dos numeros y devuelve el producto
+        """
+        pass
+    def div(self,a,b):
+        """Esta funcion recibe dos numeros y devuelve el cociente
+        >>> calc.div(2,2)
+        1
+        """
+        pass
